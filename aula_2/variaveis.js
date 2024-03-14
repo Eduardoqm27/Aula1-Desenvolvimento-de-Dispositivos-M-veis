87de32525fd3be5b5689b8e76d7d02e070667aca
Eduardoqm27 <eduardoqmarangon@gmail.com>
2024-03-14 09:21:45 -0300

    aula2_variaveis

---
+++ aula_2/variaveis.js
@@ -0,0 +1,20 @@
+var nome="Eduardo";
+var nomE='Quartiero';
+var noME=`Marangon`;
+var Nome=1.80;
+var NOme=true;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomE);
+console.log(typeof nomE);
+
+console.log(noME);
+console.log(typeof noME);
+
+console.log(Nome);
+console.log(typeof Nome);
+
+console.log(NOme);
+console.log(typeof NOme);
